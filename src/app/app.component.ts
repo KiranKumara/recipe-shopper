895a8ed125b70012e77ef52377752b7c56d67012
Kiran Kumara <kiran.kumara@sumerusolutions.com>
2019-01-02 16:07:15 -0800

    Template Driven form practice.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f') userForm: NgForm;
   title = 'recipe-shopper';
+  defaultOption = 'advanced';
+  formSubmitted = false;
+  user = {
+    enteredEmail: '',
+    selectedSubscription: ''
+  };
+
+  onSubmit() {
+    this.formSubmitted = true
+    console.log(this.userForm.value);
+    this.user.enteredEmail = this.userForm.value.email;
+    this.user.selectedSubscription = this.userForm.value.subscription;
+  }
 }
