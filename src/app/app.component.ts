956a99b0577e1ffe448a8c73fc42c300d3b55392
Kiran Kumara <kiran.kumara@sumerusolutions.com>
2019-01-05 19:19:02 -0800

    Cleanup/removed practice code.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,63 +1,10 @@
-import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
-import { NgForm, FormGroup, FormControl, Validators } from '@angular/forms';
-
-import { Observable } from 'rxjs/Observable';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  @ViewChild('f') userForm: NgForm;
+export class AppComponent {
   title = 'recipe-shopper';
-  defaultOption = 'advanced';
-  formSubmitted = false;
-  user = {
-    enteredEmail: '',
-    selectedSubscription: ''
-  };
-  projectStatuses = ['Stable', 'Critical', 'Finished'];
-  projectForm: FormGroup;
-  projectFormSubmited = false;
-
-
-  ngOnInit () {
-    this.projectForm = new FormGroup({
-      'projectName': new FormControl(null, [Validators.required, this.forbiddenProjectName], this.forbiddenProjectNameFromServer),
-      'projectEmail': new FormControl(null, [Validators.required, Validators.email]),
-      'projectStatus': new FormControl(null)
-    })
-  }
-
-  onSubmit() {
-    this.formSubmitted = true
-    console.log(this.userForm.value);
-    this.user.enteredEmail = this.userForm.value.email;
-    this.user.selectedSubscription = this.userForm.value.subscription;
-  }
-
-  onProjectFormSubmit() {
-    this.projectFormSubmited = this.projectForm.valid;
-  }
-
-  forbiddenProjectName(control: FormControl): {[s: string]: boolean} {
-    if (control.value == 'Test') {
-      return {'forbiddenProjectName': true};
-    }
-    return null;
-  }
-
-  forbiddenProjectNameFromServer(control: FormControl): Promise<any> | Observable<any> {
-    const promise = new Promise<any>((resolve, reject) => {
-      setTimeout(() => {
-        if (control.value == 'TestProjectname') {
-          resolve({'forbiddenProjectName': true});
-        } else {
-          resolve(null);
-        }
-      }, 3000);
-    });
-    return promise;
-  }
 }
